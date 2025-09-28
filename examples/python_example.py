d26cbc480d853cc5899bdb2056ae49dbb6bcf00c
tugascrown204 <gajdertevet@hotmail.com>
2025-09-28 16:20:55 +0330

    feat: initial commit for examples/python_example.py

---
+++ examples/python_example.py
@@ -0,0 +1,10 @@
+def clean_function():
+    """This function demonstrates clean code principles in Python."""
+    # Clean examples involve meaningful naming
+    total_sum = 0
+    for number in range(1, 11):
+        total_sum += number
+    return total_sum
+
+if __name__ == '__main__':
+    print(clean_function())
