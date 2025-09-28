24c70de2e91d6ff1cd583da314a78c214196b5bd
tugascrown204 <gajdertevet@hotmail.com>
2025-09-28 16:20:57 +0330

    feat: initial commit for examples/javascript_example.js

---
+++ examples/javascript_example.js
@@ -0,0 +1,10 @@
+function cleanFunction() {
+    // Clean code involves clear variable names
+    let totalSum = 0;
+    for (let number = 1; number <= 10; number++) {
+        totalSum += number;
+    }
+    return totalSum;
+}
+
+console.log(cleanFunction());
